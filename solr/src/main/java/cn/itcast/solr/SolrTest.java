292b4d8316326ba46c780a39b3be674f4e5db4a2
hujunfei <1253179427@qq.com>
2019-11-17 12:46:00 +0800

    创建一个list集合

--- solr/src/main/java/cn/itcast/solr/SolrTest.java
+++ solr/src/main/java/cn/itcast/solr/SolrTest.java
@@ -1,5 +1,7 @@
 package cn.itcast.solr;
 
+import java.util.ArrayList;
+
 public class SolrTest {
     public static void main(String[] args) {
         int[] arr = {1,2,3};
@@ -10,5 +12,10 @@ public class SolrTest {
         StringBuffer stringBuffer = new StringBuffer();
         stringBuffer.append("1231");
         System.out.println("stringBuffer = " + stringBuffer);
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("1231");
+        list.add("45532");
+        System.out.println("list = " + list);
     }
 }
