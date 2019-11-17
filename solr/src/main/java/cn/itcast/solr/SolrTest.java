3d34a3ecd5ee837a4043cb531e9ea3cc4a0d0ee3
hujunfei <1253179427@qq.com>
2019-11-17 12:25:33 +0800

    初始化项目

---
+++ solr/src/main/java/cn/itcast/solr/SolrTest.java
@@ -0,0 +1,10 @@
+package cn.itcast.solr;
+
+public class SolrTest {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
